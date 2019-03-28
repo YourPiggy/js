a858150cb3876c51c7c8cba64a8cdcb39155724e
YourPiggy <48221686+YourPiggy@users.noreply.github.com>
2019-03-28 18:47:22 +0300

    Add files via upload

--- javascript/js/hw1.js
+++ javascript/js/hw1.js
@@ -2,7 +2,7 @@ console.log("Задание 1");
 let a =  4;
 let b = 5;
 let c = 8;
-console.log(a * b * c);
+console.log('Площадь параллелипипеда'a * b * c);
 ex1 = (a >= parseInt(c)) ? "ширина больше или равна" : "высота больше";
 console.log(ex1);
 
@@ -11,7 +11,7 @@ console.log("Задание 2");
 let sotka = 1000;
 let gryadka = 15 * 25;
 
-console.log (sotka % gryadka);
+console.log ('Не занято'sotka % gryadka);
 
 console.log("Задание 3");
 
